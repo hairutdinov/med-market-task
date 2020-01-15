98ae6008c447abe40cebf9e677530c1bda3b0dbf
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2020-01-15 09:54:10 +0300

    Миграции для таблиц: категории, товары и изображения товаров

---
+++ migrations/m200115_064746_create_product_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%product}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%category}}`
+ */
+class m200115_064746_create_product_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%product}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'category_id' => $this->integer()->notNull(),
+            'price' => $this->integer(),
+            'description' => $this->text(),
+        ]);
+
+        // creates index for column `category_id`
+        $this->createIndex(
+            '{{%idx-product-category_id}}',
+            '{{%product}}',
+            'category_id'
+        );
+
+        // add foreign key for table `{{%category}}`
+        $this->addForeignKey(
+            '{{%fk-product-category_id}}',
+            '{{%product}}',
+            'category_id',
+            '{{%category}}',
+            'id',
+            'RESTRICT'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%category}}`
+        $this->dropForeignKey(
+            '{{%fk-product-category_id}}',
+            '{{%product}}'
+        );
+
+        // drops index for column `category_id`
+        $this->dropIndex(
+            '{{%idx-product-category_id}}',
+            '{{%product}}'
+        );
+
+        $this->dropTable('{{%product}}');
+    }
+}
