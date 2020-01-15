4378a81efff2b3de124961a58d8cfdec6c7d10aa
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2020-01-15 13:42:35 +0300

    Миграция для таблицы ролей

---
+++ migrations/m200115_103919_create_role_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%role}}`.
+ */
+class m200115_103919_create_role_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%role}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%role}}');
+    }
+}
