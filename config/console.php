24baa43e2525d3edd1de1ea1cf43ec1ff0a9bca8
Bulat Khayrutdinov <bulatxajrutdinoff@yandex.ru>
2020-01-16 10:48:46 +0300

    Добавил компонент authManager с использованием класса yii\rbac\DbManager

--- config/console.php
+++ config/console.php
@@ -4,30 +4,33 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic-console',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'app\commands',
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-        '@tests' => '@app/tests',
+  'id' => 'basic-console',
+  'basePath' => dirname(__DIR__),
+  'bootstrap' => ['log'],
+  'controllerNamespace' => 'app\commands',
+  'aliases' => [
+    '@bower' => '@vendor/bower-asset',
+    '@npm' => '@vendor/npm-asset',
+    '@tests' => '@app/tests',
+  ],
+  'components' => [
+    'cache' => [
+      'class' => 'yii\caching\FileCache',
     ],
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
+    'log' => [
+      'targets' => [
+        [
+          'class' => 'yii\log\FileTarget',
+          'levels' => ['error', 'warning'],
         ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
+      ],
     ],
-    'params' => $params,
+    'db' => $db,
+    'authManager' => [
+      'class' => 'yii\rbac\DbManager',
+    ],
+  ],
+  'params' => $params,
     /*
     'controllerMap' => [
         'fixture' => [ // Fixture generation command line.
@@ -41,7 +44,7 @@ if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+      'class' => 'yii\gii\Module',
     ];
 }
 
